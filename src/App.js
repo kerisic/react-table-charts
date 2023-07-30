8c1b594fd59df518ab6857b60b6ba6a9f0d43e88
Christine Chee <christinechee88@hotmail.com>
2023-07-30 18:46:15 +0100

    Create nav bar and table page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navigationbar from "./components/NavigationBar";
 import "./App.css";
 import Home from "./pages";
+import Table from "./pages/table";
 
 function App() {
   return (
     <Router>
+      <Navigationbar />
       <Routes>
         <Route exact path="/" exact element={<Home />} />
+        <Route path="/table" element={<Table />} />
       </Routes>
     </Router>
   );
---
+++ src/components/NavigationBar/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+
+const Navigationbar = () => {
+  return (
+    <Navbar bg="dark" data-bs-theme="dark">
+      <Container>
+        <Navbar.Brand href="#home">React Test</Navbar.Brand>
+        <Nav className="me-auto">
+          <Nav.Link href="/">Home</Nav.Link>
+          <Nav.Link href="/table">Table</Nav.Link>
+        </Nav>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default Navigationbar;
